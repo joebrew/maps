a9d9bdefb258af164c5ea8be73bd50b36805f431
Joe Brew <joebrew@gmail.com>
2014-08-29 16:30:40 -0400

    maps for a few countries

--- code.R
+++ code.R
@@ -17,7 +17,14 @@ MapFun <- function(country,
                    keep = FALSE,
                    border="black",
                    #fill = TRUE,
-                   col ="grey"){
+                   col ="grey",
+                   pdf = FALSE,
+                   xlim = NULL,
+                   ylim = NULL){
+  
+  par(mar=c(0,0,0,0))
+  par(oma=c(0,0,0,0))
+  
   
   #LOAD
   load(paste0(tolower(country), level, ".RData"),
@@ -28,15 +35,49 @@ MapFun <- function(country,
        }
        )
   
+  # PDF
+  if (pdf){
+    pdf(paste0(country, ".pdf"),
+        height= 3,
+        width = 3)
+    
+    if (!is.null(xlim) & !(is.null(ylim))){
+      plot(gadm, 
+           lwd = 0.2,
+           col = col,
+           border = border,
+           xlim=xlim,
+           ylim=ylim)
+    }else{
+      plot(gadm, 
+           lwd = 0.2,
+           col = col,
+           border = border)
+    }
+    dev.off()
+  }
   #PLOT
-  plot(gadm, 
-       col = col,
-       border = border)
+  if (!is.null(xlim) & !(is.null(ylim))){
+    plot(gadm, lwd = 0.2,
+         col = col,
+         border = border,
+         xlim=xlim,
+         ylim=ylim)
+  }else{
+    plot(gadm, lwd = 0.2,
+         col = col,
+         border = border)
+  }
+
 }
 
-MapFun(country = "Spain", 
-       level = 3,
+par(mar=c(0,0,0,0))
+par(oma=c(0,0,0,0))
+
+MapFun(country = "spain", 
+       level = 2,
        keep = FALSE,
        border="black",
        #fill = TRUE,
-       col ="white")
+       col = "white",
+       pdf = TRUE)
